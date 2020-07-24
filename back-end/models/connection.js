4b045e7d4a64e25742826061a25d229b454b646d
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-24 15:39:41 -0300

    Add - Validate Middleware

---
+++ back-end/models/connection.js
@@ -0,0 +1,23 @@
+const mysqlx = require("@mysql/xdevapi");
+
+const getSession = () =>
+  mysqlx.getSession({
+    user: "root",
+    password: "qwe123!@#",
+    host: "localhost",
+    port: 33060,
+    schema: "CookMaster",
+  });
+
+const connection = async () =>
+  getSession()
+    .then((session) => session.getSchema("Trybeer"))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+module.exports = {
+  connection,
+  getSession,
+};
