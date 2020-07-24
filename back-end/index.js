aebf15d15312f4c5ab2c14103d96be51ca244c39
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-24 12:43:10 -0300

    Modify - index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
+
 const express = require("express");
+
 const bodyParser = require("body-parser");
+
 const routes = require("./routes");
+
 // const middlewares = require('./middlewares');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use("/users", routes.users);
 
 // app.use(middlewares.error);
 
-app.listen(3001, () => console.log("Listening on 5000"));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
