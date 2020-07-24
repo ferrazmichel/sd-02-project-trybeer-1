0ed4713d5f86edb437bccf93df1ae6e78b90b665
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-24 12:35:58 -0300

    Bolivar Starting Feature

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 // const middlewares = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/users', routes.users);
+app.use("/users", routes.users);
 
 // app.use(middlewares.error);
 
-app.listen(3000, () => console.log('Listening on 5000'));
+app.listen(3001, () => console.log("Listening on 5000"));
