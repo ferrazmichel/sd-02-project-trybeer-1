66bbaff10f0e979522c2c40d261aa306bf849f4a
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-25 00:02:33 -0300

    Add - Login - User Controller

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -1,5 +1,23 @@
-const login = () => {
+const { users } = require("../services");
 
+function handleError(error) {
+  if (error === "user-not-found") {
+    throw Boom.badRequest("Usuário não existe");
+  }
+
+  if (error === "wrong-password") {
+    throw Boom.badRequest("Senha incorreta");
+  }
+}
+
+const login = async (req, res) => {
+  const { error, token } = await users.login(req.body);
+
+  if (error) {
+    handleError(error);
+  }
+
+  res.status(200).json({ token, error: null });
 };
 
 module.exports = {
