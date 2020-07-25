b4eaec86778e212e46ec7b877e107126c8870347
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-25 02:11:55 -0300

    Add - Register Router - App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Login from './pages/Login';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Register from "./pages/Register";
 
-function App () {
+function App() {
   return (
-    <div className="login_page">    
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
-    </BrowserRouter>
-    <p>TEST APPP</p>
+    <div className="login_page">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+      <p>TEST APPP</p>
     </div>
   );
 }
