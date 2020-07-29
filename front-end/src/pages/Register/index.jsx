054e07fd2f04007645b5a98972ccbb9d75b55a88
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-29 15:24:34 -0300

    Code Climate - 08

--- front-end/src/pages/Register/index.jsx
+++ front-end/src/pages/Register/index.jsx
@@ -10,6 +10,135 @@ import {
 } from "./service";
 import "./register.css";
 
+const nameField = ({ name, setName }) => (
+  <Form.Group className="box box60-80-90 flex-column">
+    <Form.Control
+      data-testid="name-input"
+      isInvalid={name.error}
+      isValid={!name.error && name.value}
+      onChange={(e) =>
+        handleField({
+          field: "name",
+          value: e.target.value,
+          callback: setName,
+        })
+      }
+      placeholder="name"
+      required="required"
+      type="string"
+    />
+    <Form.Control.Feedback
+      as="p"
+      data-testid="name-invalid"
+      style={{ display: !name.error ? "none" : "block" }}
+      type="invalid"
+    >
+      {name.error}
+    </Form.Control.Feedback>
+  </Form.Group>
+);
+
+const emailField = ({ email, setEmail }) => (
+  <Form.Group className="box box60-80-90 flex-column">
+    <Form.Control
+      data-testid="email-input"
+      isInvalid={email.error}
+      isValid={!email.error && email.value}
+      onChange={(e) =>
+        handleField({
+          field: "email",
+          value: e.target.value,
+          callback: setEmail,
+        })
+      }
+      placeholder="e-mail"
+      required="required"
+      type="email"
+    />
+    <Form.Control.Feedback
+      as="p"
+      data-testid="email-invalid"
+      style={{ display: !email.error ? "none" : "block" }}
+      type="invalid"
+    >
+      {email.error}
+    </Form.Control.Feedback>
+  </Form.Group>
+);
+
+const passwordField = ({ password, confirm, setPassword, setConfirm }) => (
+  <Form.Group className="box box60-80-90 flex-column">
+    <Form.Control
+      data-testid="password-input"
+      isInvalid={password.error}
+      isValid={!password.error && password.value}
+      onChange={(e) => {
+        handleField({
+          field: "password",
+          value: e.target.value,
+          callback: setPassword,
+        });
+        confirm.value &&
+          handleConfirm({
+            value: confirm.value,
+            callback: setConfirm,
+            password: e.target.value,
+          });
+      }}
+      placeholder="password"
+      required="required"
+      type="password"
+    />
+    <Form.Control.Feedback
+      as="p"
+      data-testid="password-invalid"
+      style={{ display: !password.error ? "none" : "block" }}
+      type="invalid"
+    >
+      {password.error}
+    </Form.Control.Feedback>
+  </Form.Group>
+);
+
+const confirmField = ({ confirm, setConfirm, password }) => (
+  <Form.Group className="box box60-80-90 flex-column">
+    <Form.Control
+      data-testid="confirm-input"
+      isInvalid={confirm.error}
+      isValid={!confirm.error && confirm.value}
+      onChange={(e) => {
+        handleConfirm({
+          value: e.target.value,
+          callback: setConfirm,
+          password: password.value,
+        });
+      }}
+      placeholder="confirm password"
+      required="required"
+      type="password"
+    />
+    <Form.Control.Feedback
+      as="p"
+      data-testid="confirm-invalid"
+      style={{ display: !confirm.error ? "none" : "block" }}
+      type="invalid"
+    >
+      {confirm.error}
+    </Form.Control.Feedback>
+  </Form.Group>
+);
+
+const roleField = ({ setRole }) => (
+  <Form.Group className="box box60-80-90 flex-column">
+    <Form.Check
+      id="checkbox"
+      type="checkbox"
+      label="Quero vender"
+      onChange={(e) => handleRole({ value: e.target.checked, setRole })}
+    />
+  </Form.Group>
+);
+
 function Register() {
   const history = useHistory();
   const [confirm, setConfirm] = useState({ value: null, error: null });
@@ -21,120 +150,11 @@ function Register() {
   return (
     <section className="box box60-90 flex-column flex-font">
       <Form className="box box90 flex-column">
-        <Form.Group className="box box60-80-90 flex-column">
-          <Form.Control
-            data-testid="name-input"
-            isInvalid={name.error}
-            isValid={!name.error && name.value}
-            onChange={(e) =>
-              handleField({
-                field: "name",
-                value: e.target.value,
-                callback: setName,
-              })
-            }
-            placeholder="name"
-            required="required"
-            type="string"
-          />
-          <Form.Control.Feedback
-            as="p"
-            data-testid="name-invalid"
-            style={{ display: !name.error ? "none" : "block" }}
-            type="invalid"
-          >
-            {name.error}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="box box60-80-90 flex-column">
-          <Form.Control
-            data-testid="email-input"
-            isInvalid={email.error}
-            isValid={!email.error && email.value}
-            onChange={(e) =>
-              handleField({
-                field: "email",
-                value: e.target.value,
-                callback: setEmail,
-              })
-            }
-            placeholder="e-mail"
-            required="required"
-            type="email"
-          />
-          <Form.Control.Feedback
-            as="p"
-            data-testid="email-invalid"
-            style={{ display: !email.error ? "none" : "block" }}
-            type="invalid"
-          >
-            {email.error}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="box box60-80-90 flex-column">
-          <Form.Control
-            data-testid="password-input"
-            isInvalid={password.error}
-            isValid={!password.error && password.value}
-            onChange={(e) => {
-              handleField({
-                field: "password",
-                value: e.target.value,
-                callback: setPassword,
-              });
-              confirm.value &&
-                handleConfirm({
-                  value: confirm.value,
-                  callback: setConfirm,
-                  password: e.target.value,
-                });
-            }}
-            placeholder="password"
-            required="required"
-            type="password"
-          />
-          <Form.Control.Feedback
-            as="p"
-            data-testid="password-invalid"
-            style={{ display: !password.error ? "none" : "block" }}
-            type="invalid"
-          >
-            {password.error}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="box box60-80-90 flex-column">
-          <Form.Control
-            data-testid="confirm-input"
-            isInvalid={confirm.error}
-            isValid={!confirm.error && confirm.value}
-            onChange={(e) => {
-              handleConfirm({
-                value: e.target.value,
-                callback: setConfirm,
-                password: password.value,
-              });
-            }}
-            placeholder="confirm password"
-            required="required"
-            type="password"
-          />
-          <Form.Control.Feedback
-            as="p"
-            data-testid="confirm-invalid"
-            style={{ display: !confirm.error ? "none" : "block" }}
-            type="invalid"
-          >
-            {confirm.error}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="box box60-80-90 flex-column">
-          <Form.Check
-            id="checkbox"
-            type="checkbox"
-            label="Quero vender"
-            onChange={(e) => handleRole({ value: e.target.checked, setRole })}
-          />
-        </Form.Group>
+        {nameField({ name, setName })}
+        {emailField({ email, setEmail })}
+        {passwordField({ password, confirm, setPassword, setConfirm })}
+        {confirmField({ confirm, setConfirm, password })}
+        {roleField({ setRole })}
       </Form>
       <Button
         className="box box40"
--- front-end/src/services/Request.js
+++ front-end/src/services/Request.js
@@ -11,13 +11,10 @@ const getData = async (endpoint) =>
   axios
     .get(endpoint, { headers })
     .catch((error) => ({ ...error.response.data, message: error.message }));
-<<<<<<< HEAD
 
 const postData = async ({ endpoint, body }) =>
   axios
     .post(endpoint, body)
     .catch((error) => ({ ...error.response.data, message: error.message }));
-=======
->>>>>>> master
 
 export { getData, putData, postData };
