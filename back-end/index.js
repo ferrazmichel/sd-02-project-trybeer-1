90c0650c226563212b17878e7e0af5f47969996b
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-29 10:38:32 -0300

    Modify - SQL Script

--- back-end/index.js
+++ back-end/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/images', express.static(path.join(__dirname, 'uploads')));
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 app.use('/products', products);
 
