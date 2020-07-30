f0419f724384557148aad19dae7f2c982f90448c
Andrey <andreytsuzuki@gmail.com>
2020-07-29 21:17:33 -0300

    fix

--- front-end/src/pages/Products/service.js
+++ front-end/src/pages/Products/service.js
@@ -2,15 +2,11 @@ import { getData } from "../../services/Request";
 
 const URL = "http://localhost:3001/products";
 
-const errorType = {
-  'Network Error': 'Falha no servidor!',
-};
-
 const getProducts = async () => {
   const { data , error } = await getData(URL);
 
   if (error) {
-    return { error: errorType[error], data: [] };
+    return { error, data: [] };
   }
 
   if (!data) {
