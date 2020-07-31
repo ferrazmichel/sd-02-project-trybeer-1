01191e2e7e67b741b596c0b3b2ceca5b399fdbe1
Douglas Henrique <doughrqe@gmail.com>
2020-07-30 23:09:16 -0300

    time

--- front-end/src/NotFound.js
+++ front-end/src/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const NotFound = () => (
-  <div className="not_found">
-    <h1>Pagina não encontrada</h1>
-    <img className="giphy_not_found" src="https://media2.giphy.com/media/njYrp176NQsHS/giphy-downsized-large.gif" />
-  </div>
-);
+const NotFound = ({ history }) => {
+  setTimeout(()=> history.push('/'),5500);
+  return (
+    <div className="not_found">
+      <h1>Pagina não encontrada</h1>
+      <img className="giphy_not_found" src="https://media2.giphy.com/media/njYrp176NQsHS/giphy-downsized-large.gif" />
+    </div>
+  )
+};
 export default NotFound;
