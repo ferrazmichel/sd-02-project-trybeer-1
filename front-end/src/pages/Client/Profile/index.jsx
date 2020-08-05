2ea57eb3998380986fb7caa1e7eb2c0b3184e7cb
Andrey <andreytsuzuki@gmail.com>
2020-08-05 17:14:00 -0300

    fix

--- front-end/src/pages/Client/Profile/index.jsx
+++ front-end/src/pages/Client/Profile/index.jsx
@@ -4,7 +4,6 @@ import { FormGroup, Header, Message, SubmitButton } from "../../../components";
 import Form from "react-bootstrap/Form";
 import { getUser, handleSubmit } from "./service";
 import "./style.css";
-import { message } from "../../../../../back-end/services/utils/joinSchemas/ordersSchema";
 
 const Profile = () => {
   const { message, setMessage } = useContext(Context);
